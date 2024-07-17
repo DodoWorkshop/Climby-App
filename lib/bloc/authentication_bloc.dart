38a490f3a7409ba2d7deb1f4bb36f1e6969535c1
Donovan PERSENT <dpersent@MacBook-Pro-de-Donovan.local>
2024-07-17 11:26:44 +0200

    feat(autologin): add auto login

--- lib/bloc/authentication_bloc.dart
+++ lib/bloc/authentication_bloc.dart
@@ -1,8 +1,10 @@
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:climby/client/api_client.dart';
+import 'package:climby/constant/stored_keys.dart';
 import 'package:climby/repository/authentication_repository.dart';
 import 'package:climby/util/log_utils.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'notification_bloc.dart';
 
@@ -16,7 +18,33 @@ class AuthenticationBloc
       this._authenticationRepository, this._apiClient, this._notificationBloc)
       : super(UnauthenticatedBloc(false)) {
     on<LoginEvent>(_handleLogin);
+    on<AutoLoginEvent>(_handleAutoLogin);
     on<LogoutEvent>(_handleLogout);
+  }
+
+  void _handleAutoLogin(
+      AutoLoginEvent event, Emitter<AuthenticationBlocState> emit) async {
+    LogUtils.log("Attempting auto login...");
+
+    emit(UnauthenticatedBloc(true));
+
+    final prefs = await SharedPreferences.getInstance();
+
+    final jwt = prefs.getString(JWT_KEY);
+    if (jwt != null) {
+      final userProfile = await _authenticationRepository.getUserProfile(jwt);
+
+      _apiClient.token = jwt;
+
+      final newState = AuthenticatedState(userProfile, jwt);
+
+      emit(newState);
+
+      _notificationBloc
+          .add(SendSuccessNotificationEvent("Reconnexion réussie"));
+    } else {
+      emit(UnauthenticatedBloc(false));
+    }
   }
 
   void _handleLogin(
@@ -25,11 +53,11 @@ class AuthenticationBloc
 
     try {
       final credentials = await _authenticationRepository.login();
+
+      await _setCredentials(credentials);
+
       final newState =
           AuthenticatedState(credentials.user, credentials.accessToken);
-
-      // Set api token
-      _apiClient.token = newState.jwt;
 
       emit(newState);
 
@@ -49,11 +77,33 @@ class AuthenticationBloc
 
       // Set api token
       _apiClient.token = null;
+
+      // Remove stored keys
+      final prefs = await SharedPreferences.getInstance();
+      prefs.remove(REFRESH_TOKEN_KEY);
+      prefs.remove(JWT_KEY);
     } catch (e) {
       LogUtils.logError(e);
     }
 
     emit(UnauthenticatedBloc(false));
+  }
+
+  Future<void> _setCredentials(Credentials credentials) async {
+    // Set api token
+    final jwt = credentials.accessToken;
+    final refreshToken = credentials.refreshToken;
+
+    _apiClient.token = jwt;
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(JWT_KEY, jwt);
+
+    if (refreshToken != null) {
+      prefs.setString(REFRESH_TOKEN_KEY, refreshToken);
+    } else {
+      LogUtils.log("Refresh token is null");
+    }
   }
 }
 
@@ -62,6 +112,8 @@ sealed class AuthenticationEvent {
 }
 
 class LoginEvent extends AuthenticationEvent {}
+
+class AutoLoginEvent extends AuthenticationEvent {}
 
 class LogoutEvent extends AuthenticationEvent {}
 
---
+++ lib/constant/stored_keys.dart
@@ -0,0 +1,2 @@
+const JWT_KEY = "auth_jwt";
+const REFRESH_TOKEN_KEY = "auth_refresh_token";
